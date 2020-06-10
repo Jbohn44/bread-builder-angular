4f09c44b65d5e2c283fd8ea43089c097da84d853
Jbohn44 <ejbohnert@yahoo.com>
2020-06-10 18:12:36 -0500

    added formula components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashPageComponent } from './splash-page/splash-page/splash-page.component';
+import { FormulaPageComponent } from './formula/formula-page/formula-page.component';
+import { FormulaFormComponent } from './formula/formula-form/formula-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashPageComponent
+    SplashPageComponent,
+    FormulaPageComponent,
+    FormulaFormComponent
   ],
   imports: [
     BrowserModule,
