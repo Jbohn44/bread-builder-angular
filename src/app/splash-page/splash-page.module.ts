df6104fe6ccc260a51571b2a5d73b7e4e2cf0a39
Jbohn44 <ejbohnert@yahoo.com>
2020-06-14 10:56:57 -0500

    added user components

--- src/app/splash-page/splash-page.module.ts
+++ src/app/splash-page/splash-page.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SplashPageRoutingModule } from './splash-page-routing.module';
 import { SplashComponent } from './splash/splash.component';
+import { UserComponent } from './user/user.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 
 
 @NgModule({
-  declarations: [SplashComponent],
+  declarations: [SplashComponent, UserComponent, UserLoginComponent, UserSignUpComponent],
   imports: [
     CommonModule,
     SplashPageRoutingModule
